97af1e41ce0170d8aeca5b7a7327569a0e4e47e9
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-12 16:21:05 -0600

    feat(RV32I.sv): Adding top-level module

---
+++ src/RV32I.sv
@@ -0,0 +1,16 @@
+`include "RV32I_defines.v"
+import fe_pkg::*;
+
+module RV32I (
+    input wire [`RV32I_INSTRUCTION_WIDTH-1:0] raw_bits,
+
+    output RV32I_OPCODE_t opcode,
+    output RV32I_RS1_t rs1
+);
+
+RV32I_decoder decoder(
+    .raw_bits(raw_bits),
+    .rs1(rs1)
+);
+    
+endmodule : RV32I
