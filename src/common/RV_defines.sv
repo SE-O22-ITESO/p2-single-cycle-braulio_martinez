cd1c1cdd39544cb858c9d3d3b50a5e1ba82c3909
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-12 14:50:17 -0600

    feat(RV_defines.sv): Create file with common defines
    Add RV32I prefix to limit defines to specific instructions

---
+++ src/common/RV_defines.sv
@@ -0,0 +1,8 @@
+`define RV32I_OPCODE_WIDTH        7
+`define RV32I_INSTRUCTION_WIDTH   32
+`define RV32I_FUNCT_7_WIDTH       7
+`define RV32I_FUNCT_3_WDITH       3
+`define RV32I_RS1_ADDR_WIDTH      5
+`define RV32I_RS2_ADDR_WIDTH      5
+`define RV32I_RD_ADDR_WIDTH       5
+`define RV32I_IMM_WIDTH           32
