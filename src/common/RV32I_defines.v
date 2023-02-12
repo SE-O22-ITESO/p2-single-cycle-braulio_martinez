d9a90805cc6c884b6ff63f3fcf6c888ea121e01e
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-12 16:20:33 -0600

    fix(RV32I_defines): Rename (again)

--- src/common/RV32I_defines.v
+++ src/common/RV32I_defines.v
@@ -1,7 +1,7 @@
 `define RV32I_OPCODE_WIDTH        7
 `define RV32I_INSTRUCTION_WIDTH   32
 `define RV32I_FUNCT_7_WIDTH       7
-`define RV32I_FUNCT_3_WDITH       3
+`define RV32I_FUNCT_3_WIDTH       3
 `define RV32I_RS1_ADDR_WIDTH      5
 `define RV32I_RS2_ADDR_WIDTH      5
 `define RV32I_RD_ADDR_WIDTH       5
