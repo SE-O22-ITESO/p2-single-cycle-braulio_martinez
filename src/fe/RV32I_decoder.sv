e0940ea314439e59ebb4381f628ebcd608944626
Martinez Aceves <braulio.i.martinez.aceves@intel.com>
2023-02-13 23:44:46 -0600

    fix(RV32I_decoder): Progress with decoding

--- src/fe/RV32I_decoder.sv
+++ src/fe/RV32I_decoder.sv
@@ -44,40 +44,40 @@ always @ *
             imm <= '0;
         end
 
-        I_TYPE: begin
+        I_TYPE, I_LOAD_TYPE, I_JALR_TYPE, I_ENV_TYPE: begin
             rs1 <= raw_bits[19:15];
             rs2 <= '0;
             rd  <= raw_bits[11:7];
-            imm <= { {20{1'b0}}, raw_bits[31:20] };
+            imm <= { {20{raw_bits[31]}}, raw_bits[31:20] };
         end
 
         S_TYPE: begin
             rs1 <= raw_bits[19:15];
             rs2 <= raw_bits[24:20];
             rd  <= '0;
-            imm <= { {20{1'b0}}, raw_bits[31:25], raw_bits[11:7] };
+            imm <= { {20{raw_bits[31]}}, raw_bits[31:25], raw_bits[11:7] };
         end
         
-        /*B_TYPE: begin
-            rs1 <= raw_bits[:];
-            rs2 <= raw_bits[:];
-            rd  <= raw_bits[:];
-            imm <= raw_bits[:];
+        B_TYPE: begin
+            rs1 <= raw_bits[24:20];
+            rs2 <= raw_bits[19:15];
+            rd  <= '0;
+            imm <= { {20{raw_bits[31]}}, raw_bits[31], raw_bits[7], raw_bits[30:25], raw_bits[11:6], 1'b0 };
         end
 
-        U_TYPE: begin
-            rs1 <= raw_bits[:];
-            rs2 <= raw_bits[:];
-            rd  <= raw_bits[:];
-            imm <= raw_bits[:];
+        U_LUI_TYPE, U_AUI_TYPE: begin
+            rs1 <= '0;
+            rs2 <= '0;
+            rd  <= raw_bits[11:7];
+            imm <= { raw_bits[31:12], {12{1'b0}} };
         end
 
         J_TYPE: begin
-            rs1 <= raw_bits[:];
-            rs2 <= raw_bits[:];
-            rd  <= raw_bits[:];
-            imm <= raw_bits[:];
-        end*/
+            rs1 <= '0;
+            rs2 <= '0;
+            rd  <= raw_bits[11:7];
+            imm <= { {12{raw_bits[31]}}, raw_bits[31], raw_bits[19:12], raw_bits[20], raw_bits[30:21], 1'b0 };
+        end
 
         default: begin
             rs1 <= '0;
@@ -92,24 +92,85 @@ always @ *
 always @*
     case (opcode)
 
-        I_TYPE: begin
+        R_TYPE:
+            case (funct3)
+                'h0: mnemonic <= (funct7[5] == '0) ?
+                                 ADD :
+                                 (funct7[5] == '1) ?
+                                 SUB : NULL;
+                'h4: mnemonic <= XOR;
+                'h6: mnemonic <= OR;
+                'h7: mnemonic <= AND;
+                'h1: mnemonic <= SLL;
+                'h5: mnemonic <= (funct7[5] == '0) ?
+                                 SRL :
+                                 (funct7[5] == '1) ?
+                                 SRA : NULL;
+                'h2: mnemonic <= SLT;
+                'h3: mnemonic <= SLTU;
+                default : mnemonic <= NULL;
+            endcase
+
+        I_TYPE:
             case (funct3)
                 'h0: mnemonic <= ADDI;
                 'h4: mnemonic <= XORI;
                 'h6: mnemonic <= ORI;
                 'h7: mnemonic <= ANDI;
                 'h1: mnemonic <= SLLI;
-                //FIXME: Might optimize by just checking 1 bit
-                'h5: mnemonic <= (funct7 == `RV32I_FUNCT_7_WIDTH'h0) ?
+                'h5: mnemonic <= (funct7[5] == '0) ?
                                  SRLI :
-                                 (funct7 == `RV32I_FUNCT_7_WIDTH'h20) ?
-                                 SRAI :
-                                 NULL;
+                                 (funct7[5] == '1) ?
+                                 SRAI : NULL;
                 'h2: mnemonic <= SLTI;
                 'h3: mnemonic <= SLTIU;
                 default : mnemonic <= NULL;
             endcase
-        end
+
+        I_LOAD_TYPE:
+            case (funct3)
+                'h0: mnemonic <= LB;
+                'h1: mnemonic <= LH;
+                'h2: mnemonic <= LW;
+                'h4: mnemonic <= LBU;
+                'h5: mnemonic <= LHU;
+                default : mnemonic <= NULL;
+            endcase
+        
+        S_TYPE:
+            case (funct3)
+                'h0: mnemonic <= SB;
+                'h1: mnemonic <= SH;
+                'h2: mnemonic <= SW;
+                default : mnemonic <= NULL;
+            endcase
+
+        B_TYPE:
+            case (funct3)
+                'h0: mnemonic <= BEQ;
+                'h1: mnemonic <= BNE;
+                'h4: mnemonic <= BLT;
+                'h5: mnemonic <= BGE;
+                'h6: mnemonic <= BLTU;
+                'h7: mnemonic <= BGEU;
+                default : mnemonic <= NULL;
+            endcase
+
+        J_TYPE:
+            mnemonic <= JAL;
+
+        I_JALR_TYPE:
+            mnemonic <= JALR;
+
+        U_LUI_TYPE:
+            mnemonic <= LUI;
+
+        U_AUI_TYPE:
+            mnemonic <= AUIPC;
+
+        I_ENV_TYPE:
+            mnemonic <= (funct7[0] == '1) ?
+                        EBREAK : ECALL;
 
         default:
             mnemonic = NULL;
