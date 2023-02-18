368351a607b3acf5d1bcee63f5b44e7e68607f1e
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-18 13:22:06 -0600

    fix(RV32I_tb): Rename and typos

--- src/val/RV32I_tb.sv
+++ src/val/RV32I_tb.sv
@@ -1,4 +1,4 @@
-`include "RV32I_defines.v"
+`include "RV32I_defines.sv"
 
 import fe_pkg::*;
 
@@ -7,23 +7,23 @@ module RV32I_tb ();
     parameter TOTAL_INSTRUCTIONS = 18;
     reg [`RV32I_INSTRUCTION_WIDTH-1:0] program_mem [TOTAL_INSTRUCTIONS];
 
-    reg [`RV32I_INSTRUCTION_WIDTH-1:0] raw_bits = '0;
+    reg [`RV32I_INSTRUCTION_WIDTH-1:0] instruction = '0;
 
     RV32I dut (
         .clk(clk),
         .rst(rst),
-        .raw_bits(raw_bits)
+        .instruction(instruction)
     );
 
     initial begin
         $readmemb("program.txt", program_mem);
 
         for (int i = 0; i < TOTAL_INSTRUCTIONS; i++) begin
-            raw_bits = program_mem[i];
+            instruction = program_mem[i];
             #1;
             $display("OPCODE: %s, INSTR: %s RS1: %x, RS2: %x, RD: %x, IMM: %x , $signed(IMM): %d",
                 dut.core.decoder.opcode.name(), dut.core.decoder.mnemonic.name(),
-                dut.core.decoder.rs1, dut.core.decoder.rs2, dut.core.decoder.rd, dut.core.decoder.imm, $signed(dut.core.decoder.imm)
+                dut.core.decoder.rs1_addr, dut.core.decoder.rs2_addr, dut.core.decoder.rd_addr, dut.core.decoder.imm, $signed(dut.core.decoder.imm)
             );
         end
         $stop();
