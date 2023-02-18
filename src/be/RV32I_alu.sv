6bdb5efc613071548de07b6425a01674652422ff
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-18 15:34:32 -0600

    fix(ALU): Use proper package Fix alu_mode, 1 for ADD, 0 for substract

--- src/be/RV32I_alu.sv
+++ src/be/RV32I_alu.sv
@@ -2,13 +2,25 @@
 // Takes parametrized inputs
 // A/B muxing must be done in previous stages
 `include "RV32I_defines.sv"
-import fe_pkg::*;
+import be_pkg::*;
 
 module RV32I_alu (
     input RV32I_OPERAND_t a, b,
     input RV32I_ALU_OP_t alu_op,
 
     output RV32I_OPERAND_t out
+);
+
+wire adder_substracter_mode =   alu_op == ADD_alu ?
+                                '1 : '0;
+
+adder_substracter # (
+    .NUMBER_OF_BITS(`RV32I_INSTRUCTION_WIDTH)
+) adder_substracter (
+    .a(a),
+    .b(b),
+    .mode(adder_substracter_mode),
+    .result(out)
 );
     
 endmodule : RV32I_alu
