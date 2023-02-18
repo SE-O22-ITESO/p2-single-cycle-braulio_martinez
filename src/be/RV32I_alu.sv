1c0ae5df8eb8664a3dea4b83b4214e0418226201
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-18 13:20:13 -0600

    fix(RV32I_alu): Fix in operand sizes to actual 32-bit signals

--- src/be/RV32I_alu.sv
+++ src/be/RV32I_alu.sv
@@ -1,12 +1,14 @@
 // RV32I_alu
 // Takes parametrized inputs
 // A/B muxing must be done in previous stages
+`include "RV32I_defines.sv"
+import fe_pkg::*;
 
 module RV32I_alu (
-    input [`RV32I_INSTRUCTION_WIDTH-1:0] a,
-    input [`RV32I_INSTRUCTION_WIDTH-1:0] b,
+    input RV32I_OPERAND_t a, b,
+    input RV32I_ALU_OP_t alu_op,
 
-    output [`RV32I_INSTRUCTION_WIDTH-1:0] out
+    output RV32I_OPERAND_t out
 );
     
 endmodule : RV32I_alu
