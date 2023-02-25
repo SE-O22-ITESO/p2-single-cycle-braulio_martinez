48f25053a55332d16969a4690a14b587f0829e89
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-25 10:19:43 -0600

    fix(Control_Unit): Reame to include STAGE number, fixes to Program Counter

--- src/fe/control_unit.sv
+++ src/fe/control_unit.sv
@@ -10,58 +10,45 @@ module control_unit (
     input RV32I_IMM_t  imm,
     input RV32I_OPCODE_t opcode,
     input RV32I_INSTRUCTION_MNEMONIC_t mnemonic,
-    input RV32I_OPERAND_t alu_out, program_counter,
+    input RV32I_OPERAND_t alu_out, program_counter_s1,
+    input RV32I_OPERAND_t alu_out_exec_s3,
 
-    output RV32I_ALU_OP_t alu_op,
-    output RV32I_OPERAND_t alu_a, alu_b, program_counter_new,
+    output logic rf_wren,
+    output RV32I_OPERAND_t alu_a, alu_b, program_counter_new, program_counter_plus_4_s1,
+    output RV32I_OPERAND_t rf_write_data,
     output RV32I_CONTROL_UNIT_FSM_t control_unit_state, control_unit_state_next
 );
-
-RV32I_OPERAND_t program_counter_plus_4;
 
 // FSM
 `FF_D_RST_EN_DATA_TYPE(clk, rst, ~rst, control_unit_state_next, control_unit_state, RV32I_CONTROL_UNIT_FSM_t)
 // Save PC+1
-`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), alu_out, program_counter_plus_4)
+`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), alu_out, program_counter_plus_4_s1)
+// Save COND operation result (rs1 - rs2)
 
 // Program counter selection logic
 always_comb
     case (opcode)
         // OPCodes that DO change the PC
         B_TYPE, J_TYPE, I_JALR_TYPE, I_ENV_TYPE:
-            program_counter_new <= alu_out;
+            program_counter_new <= alu_out_exec_s3;
         
         default:
-            program_counter_new <= program_counter_plus_4;
+            program_counter_new <= program_counter_plus_4_s1;
     endcase
 
 
-// ALU for EXEC stage only
-RV32I_ALU_OP_t  alu_op_exec;
+// ALU operands for EXEC stage only
+// ALU is used during FETCH to increase PC
 RV32I_OPERAND_t alu_exec_a, alu_exec_b;
 
-// Select final alu_op depending on current stage:
-assign alu_op   =   control_unit_state == EXECUTE_S3 ?
-                    alu_op_exec :
-                    // Force ALU to sum PC+1 
-                    control_unit_state == FETCH_S1 ?
-                    ADD_alu     :
-                    control_unit_state == DECODE_S2 ?
-                    SUB_alu     : NULL_alu;
 assign alu_a    =   control_unit_state == EXECUTE_S3 ?
                     alu_exec_a  : 
                     control_unit_state == FETCH_S1 ?
-                    program_counter :
-                    control_unit_state == DECODE_S2 ?
-                    rs1  : '0;
+                    program_counter_s1 : '0;
 assign alu_b    =   control_unit_state == EXECUTE_S3 ?
                     alu_exec_b  : 
                     control_unit_state == FETCH_S1 ?
-                    `RV32I_PC_STEP :
-                    control_unit_state == DECODE_S2 ?
-                    rs2  : '0;
-
-// ALU operands selection
+                    `RV32I_PC_STEP  : '0;
 
 
 // FSM logic
@@ -79,23 +66,6 @@ always_comb
         endcase
     end
 
-// Set alu_op_exec based on mnemonic
-always_comb
-    case (mnemonic)
-        ADD, ADDI   : alu_op_exec <= ADD_alu;
-        SUB         : alu_op_exec <= SUB_alu;
-        XOR, XORI   : alu_op_exec <= XOR_alu;
-        OR, ORI     : alu_op_exec <= OR_alu;
-        AND, ANDI   : alu_op_exec <= AND_alu;
-        SLL, SLLI   : alu_op_exec <= SLL_alu;
-        SRL, SRLI   : alu_op_exec <= SRL_alu;
-        SLTU, SLTIU : alu_op_exec <= SLTU_alu;
-        JAL         : alu_op_exec <= ADD_alu;
-
-        default:
-            alu_op_exec <= NULL_alu;
-    endcase
-
 // Mux A/B inputs to ALU depending on OPCODE
 always_comb
     case (opcode)
@@ -110,9 +80,19 @@ always_comb
             alu_exec_b <= imm;
         end
 
-        J_TYPE: begin
-            alu_exec_a <= program_counter;
+        J_TYPE, B_TYPE: begin
+            alu_exec_a <= program_counter_s1;
             alu_exec_b <= imm;
+        end
+
+        U_LUI_TYPE: begin
+            alu_exec_a <= imm;
+            alu_exec_b <= 'd12;
+        end
+
+        U_AUI_TYPE: begin
+            alu_exec_a <= imm;
+            alu_exec_b <= program_counter_s1;
         end
 
         default: begin
@@ -120,5 +100,34 @@ always_comb
             alu_exec_b <= '0;
         end
     endcase
-    
+
+// RD Write enable and data
+always_comb
+    case (control_unit_state)
+
+        WRITEBACK_S4:
+            case (opcode)
+                R_TYPE, I_TYPE, I_LOAD_TYPE, U_AUI_TYPE, U_LUI_TYPE: begin
+                    rf_wren         <= '1;
+                    rf_write_data   <= alu_out_exec_s3;
+                end
+
+                I_JALR_TYPE, J_TYPE: begin
+                    rf_wren         <= '1;
+                    rf_write_data   <= program_counter_plus_4_s1;
+                end
+
+                default: begin
+                    rf_wren         <= '0;
+                    rf_write_data   <= '0;
+                end
+            endcase
+
+        default: begin
+            rf_wren         <= '0;
+            rf_write_data   <= '0;
+        end
+
+    endcase
+
 endmodule : control_unit
