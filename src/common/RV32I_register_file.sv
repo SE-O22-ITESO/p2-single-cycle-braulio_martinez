e779f662b585c5de7065b6946d77afd58aa49d4e
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-25 10:18:20 -0600

    fix(register_file): Clear reg values on rst

--- src/common/RV32I_register_file.sv
+++ src/common/RV32I_register_file.sv
@@ -1,6 +1,6 @@
 module RV32I_register_file #(parameter NUM_OF_SETS = 32, parameter DATA_BUS_WIDTH = 32) (
 	//Inputs
-	input wire clk,
+	input wire clk, rst,
 	input wire wr_enable,
 	input wire [$clog2(NUM_OF_SETS)-1:0] rd_addr_1, rd_addr_2, rd_addr_3, wr_addr,
 	input wire [DATA_BUS_WIDTH-1:0] wr_data,
@@ -20,8 +20,17 @@ module RV32I_register_file #(parameter NUM_OF_SETS = 32, parameter DATA_BUS_WIDT
 						'0 : registers[rd_addr_3];
 
 	//Write is synced, ignore address '0
-	always @(posedge clk)
-		if (wr_enable)
-			registers[wr_addr] <= wr_data;
+	generate
+	always @(posedge clk, posedge rst) begin
+		if (rst) begin
+			for (int i=0; i<NUM_OF_SETS; i=i+1) begin : quartus_generate_fix
+				registers[i] <= '0;
+			end
+		end else begin
+			if (wr_enable)
+				registers[wr_addr] <= wr_data;
+		end
+	end
+	endgenerate
 
 endmodule : RV32I_register_file
