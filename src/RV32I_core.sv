c8f5303f8b1eda6ade8f771c2fe014f4aee919ee
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-25 12:30:16 -0600

    fix(Core): Re-structure data path, move it out of CTRL Unit
    Add correct flops for S2, move WB_S4 to S5

--- src/RV32I_core.sv
+++ src/RV32I_core.sv
@@ -12,32 +12,22 @@ module RV32I_core (
     output RV32I_OPERAND_t program_counter_s1
 );
 
-RV32I_OPERAND_t     rs1, rs2, rd, imm, alu_a, alu_b, alu_out;
-RV32I_OPERAND_t     raw_bits_fetched_s1, alu_out_exec_s3;
-RV32I_OPERAND_t     program_counter_new, program_counter_plus_4_s1;
-RV32I_OPCODE_t      opcode;
-RV32I_RS1_t         rs1_addr;
-RV32I_RS2_t         rs2_addr;
-RV32I_RD_t          rd_addr;
-RV32I_ALU_OP_t      alu_op;
-RV32I_CONTROL_UNIT_FSM_t control_unit_state;
-RV32I_INSTRUCTION_MNEMONIC_t mnemonic;
+`include "RV32I_core_internal_decls.sv"
+`include "RV32I_core_flops.sv"
 
-wire rf_wren;
-RV32I_OPERAND_t     rf_write_data;
-
-// Fetch raw_bits
-`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), raw_bits, raw_bits_fetched_s1)
-// Flop ALU results
-`FF_D_RST_EN(clk, rst, (control_unit_state == EXECUTE_S3), alu_out, alu_out_exec_s3)
-// Write new program counter
-`FF_D_RST_EN_RESET_VALUE(clk, rst, (control_unit_state == WRITEBACK_S4), program_counter_new, program_counter_s1, `RV32I_INSTRUCTION_WIDTH'h4000000)
-
+//DEBUG logic so Quartus does not skip synthesis
 assign opcode_out_debug = RV32I_OPCODE_t'(rs1 ^ 7'b1100110);
 assign rs1_out_debug = rs1;
 
+// Combo assignments
+assign opcode_changes_program_counter = (opcode_s2 == B_TYPE) || (opcode_s2 == J_TYPE) || (opcode_s2 == I_JALR_TYPE) || (opcode_s2 == I_ENV_TYPE);
+
+// Program counter selection logic
+`MUX_2_TO_1(alu_out_s3, program_counter_plus_4_s1, opcode_changes_program_counter, program_counter_new)
+
 RV32I_decoder decoder (
-    .raw_bits   (raw_bits_fetched_s1),
+    .raw_bits   (raw_bits_s1),
+
     .rs1_addr   (rs1_addr),
     .rs2_addr   (rs2_addr),
     .rd_addr    (rd_addr),
@@ -49,34 +39,44 @@ RV32I_decoder decoder (
 control_unit  control_unit  (
     .clk        (clk),
     .rst        (rst),
-    .rs1        (rs1),
-    .rs2        (rs2),
-    .rd         (rd),
-    .imm        (imm),
-    .opcode     (opcode),
-    .mnemonic   (mnemonic),
-    .alu_out    (alu_out),
-    .program_counter_s1 (program_counter_s1),
-    .program_counter_new (program_counter_new),
-    .program_counter_plus_4_s1 (program_counter_plus_4_s1),
-    .alu_out_exec_s3        (alu_out_exec_s3),
+    .opcode     (opcode_s2),
+    .mnemonic   (mnemonic_s2),
 
-    .rf_write_data (rf_write_data),
     .rf_wren    (rf_wren),
-    .alu_a      (alu_a),
-    .alu_b      (alu_b),
     .control_unit_state (control_unit_state)
+);
+
+alu_inputs_mux alu_inputs_mux (
+    .rs1        (rs1_s2),
+    .rs2        (rs2_s2),
+    .rd         (rd_s2),
+    .imm        (imm_s2),
+    .opcode     (opcode_s2),
+    .program_counter    (program_counter_s1),
+    .control_unit_state (control_unit_state),
+
+    .alu_a      (alu_a),
+    .alu_b      (alu_b)
 );
 
 RV32I_alu alu (
     .a          (alu_a),
     .b          (alu_b),
-    .rs1        (rs1),
-    .rs2        (rs2),
-    .imm        (imm),
-    .mnemonic   ((control_unit_state == EXECUTE_S3) ? mnemonic : ADD),
+    .rs1        (rs1_s2),
+    .rs2        (rs2_s2),
+    .imm        (imm_s2),
+    // Force ALU to ADD during other stages for PC+1
+    .mnemonic   ((control_unit_state == EXECUTE_S3) ? mnemonic_s2 : ADD),
     .program_counter_plus_4_s1 (program_counter_plus_4_s1),
     .out        (alu_out)
+);
+
+rf_inputs_mux rf_inputs_mux (
+    .alu_out    (alu_out_s3),
+    .program_counter_plus_4 (program_counter_plus_4_s1),
+    .opcode     (opcode_s2),
+
+    .rf_write_data  (rf_write_data)
 );
 
 RV32I_register_file #  (
@@ -88,9 +88,9 @@ RV32I_register_file #  (
     .wr_enable  (rf_wren),
     .wr_addr    (rd_addr),
     .wr_data    (rf_write_data),
-    .rd_addr_1  (rs1_addr),
-    .rd_addr_2  (rs2_addr),
-    .rd_addr_3  (rd_addr),
+    .rd_addr_1  (rs1_addr_s2),
+    .rd_addr_2  (rs2_addr_s2),
+    .rd_addr_3  (rd_addr_s2),
     .rd_data_1  (rs1),
     .rd_data_2  (rs2),
     .rd_data_3  (rd)
