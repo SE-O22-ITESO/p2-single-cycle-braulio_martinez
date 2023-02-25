60631bf53dea71667fdc38d28279be22252b2933
bimartin-iteso <braulio.martinez@iteso.mx>
2023-02-25 10:18:06 -0600

    fix(top): Change names to include STAGE number, clear REGFILE on reset

--- src/RV32I.sv
+++ src/RV32I.sv
@@ -4,18 +4,27 @@ import fe_pkg::*;
 module RV32I (
     input wire clk,
     input wire rst,
-    input RV32I_OPERAND_t instruction,
 
-    output RV32I_OPCODE_t opcode,
-    output RV32I_OPERAND_t rs1, program_counter
+    output wire program_counter
 );
+
+RV32I_OPERAND_t core_program_counter, instruction, program_mem_address;
+assign program_counter = core_program_counter && 32'hdead_beef;
+assign program_mem_address = (core_program_counter - 32'h4000000) >> 2;
+assign instruction = program_mem[program_mem_address];
+
+parameter TOTAL_INSTRUCTIONS = 18;
+reg [`RV32I_INSTRUCTION_WIDTH-1:0] program_mem [TOTAL_INSTRUCTIONS];
+
+initial
+    $readmemb("program.txt", program_mem);
 
 RV32I_core core(
     .clk(clk),
     .rst(rst),
     .raw_bits(instruction),
 
-    .program_counter(program_counter),
+    .program_counter(core_program_counter),
     .opcode_out_debug(opcode),
     .rs1_out_debug(rs1)
 );
--- src/RV32I_core.sv
+++ src/RV32I_core.sv
@@ -9,11 +9,12 @@ module RV32I_core (
 
     output RV32I_OPCODE_t opcode_out_debug,
     output RV32I_OPERAND_t rs1_out_debug,
-    output RV32I_OPERAND_t program_counter
+    output RV32I_OPERAND_t program_counter_s1
 );
 
-RV32I_OPERAND_t     rs1, rs2, rd, raw_bits_fetched, imm, alu_a, alu_b, alu_out;
-RV32I_OPERAND_t     program_counter_new;
+RV32I_OPERAND_t     rs1, rs2, rd, imm, alu_a, alu_b, alu_out;
+RV32I_OPERAND_t     raw_bits_fetched_s1, alu_out_exec_s3;
+RV32I_OPERAND_t     program_counter_new, program_counter_plus_4_s1;
 RV32I_OPCODE_t      opcode;
 RV32I_RS1_t         rs1_addr;
 RV32I_RS2_t         rs2_addr;
@@ -22,16 +23,21 @@ RV32I_ALU_OP_t      alu_op;
 RV32I_CONTROL_UNIT_FSM_t control_unit_state;
 RV32I_INSTRUCTION_MNEMONIC_t mnemonic;
 
+wire rf_wren;
+RV32I_OPERAND_t     rf_write_data;
+
 // Fetch raw_bits
-`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), raw_bits, raw_bits_fetched)
+`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), raw_bits, raw_bits_fetched_s1)
+// Flop ALU results
+`FF_D_RST_EN(clk, rst, (control_unit_state == EXECUTE_S3), alu_out, alu_out_exec_s3)
 // Write new program counter
-`FF_D_RST_EN_RESET_VALUE(clk, rst, (control_unit_state == EXECUTE_S3), program_counter_new, program_counter, `RV32I_INSTRUCTION_WIDTH'h4000000)
+`FF_D_RST_EN_RESET_VALUE(clk, rst, (control_unit_state == WRITEBACK_S4), program_counter_new, program_counter_s1, `RV32I_INSTRUCTION_WIDTH'h4000000)
 
 assign opcode_out_debug = RV32I_OPCODE_t'(rs1 ^ 7'b1100110);
 assign rs1_out_debug = rs1;
 
 RV32I_decoder decoder (
-    .raw_bits   (raw_bits_fetched),
+    .raw_bits   (raw_bits_fetched_s1),
     .rs1_addr   (rs1_addr),
     .rs2_addr   (rs2_addr),
     .rd_addr    (rd_addr),
@@ -50,20 +56,27 @@ control_unit  control_unit  (
     .opcode     (opcode),
     .mnemonic   (mnemonic),
     .alu_out    (alu_out),
-    .program_counter (program_counter),
+    .program_counter_s1 (program_counter_s1),
     .program_counter_new (program_counter_new),
+    .program_counter_plus_4_s1 (program_counter_plus_4_s1),
+    .alu_out_exec_s3        (alu_out_exec_s3),
 
-    .alu_op     (alu_op),
+    .rf_write_data (rf_write_data),
+    .rf_wren    (rf_wren),
     .alu_a      (alu_a),
     .alu_b      (alu_b),
     .control_unit_state (control_unit_state)
 );
 
 RV32I_alu alu (
-    .a      (alu_a),
-    .b      (alu_b),
-    .alu_op (alu_op),
-    .out    (alu_out)
+    .a          (alu_a),
+    .b          (alu_b),
+    .rs1        (rs1),
+    .rs2        (rs2),
+    .imm        (imm),
+    .mnemonic   ((control_unit_state == EXECUTE_S3) ? mnemonic : ADD),
+    .program_counter_plus_4_s1 (program_counter_plus_4_s1),
+    .out        (alu_out)
 );
 
 RV32I_register_file #  (
@@ -71,9 +84,10 @@ RV32I_register_file #  (
     .DATA_BUS_WIDTH (`RV32I_INSTRUCTION_WIDTH)
 ) register_file (
     .clk        (clk),
-    .wr_enable  ('0),
+    .rst        (rst),
+    .wr_enable  (rf_wren),
     .wr_addr    (rd_addr),
-    .wr_data    ('0),
+    .wr_data    (rf_write_data),
     .rd_addr_1  (rs1_addr),
     .rd_addr_2  (rs2_addr),
     .rd_addr_3  (rd_addr),
