e3a8ada65f22ec108c0e375d659ac9b222d0b244
bimartin-iteso <braulio.martinez@iteso.mx>
2023-03-04 09:30:09 -0600

    fix(flops/internal signals): Correctly stage signals

--- src/RV32I_core_internal_decls.sv
+++ src/RV32I_core_internal_decls.sv
@@ -1,7 +1,7 @@
 RV32I_OPERAND_t                 rs1, rs2, rd, imm, alu_a, alu_b, alu_out;
 RV32I_OPERAND_t                 rs1_s2, rs2_s2, rd_s2, imm_s2;
 RV32I_OPERAND_t                 raw_bits_s1, alu_out_s3;
-RV32I_OPERAND_t                 program_counter_s1, program_counter_new, program_counter_plus_4_s1;
+RV32I_OPERAND_t                 program_counter_s1, program_counter_new, program_counter_plus_4_s1, program_counter_plus_imm_s2;
 RV32I_OPERAND_t                 rf_write_data;
 RV32I_OPERAND_t                 bus_rddata_s4;
 RV32I_OPCODE_t                  opcode, opcode_s2;
@@ -13,3 +13,5 @@ RV32I_CONTROL_UNIT_FSM_t        control_unit_state;
 RV32I_INSTRUCTION_MNEMONIC_t    mnemonic, mnemonic_s2;
 
 wire                            rf_wren, opcode_changes_program_counter, bus_addr_select_alu_out;
+wire                            program_counter_wren;
+logic                           cond_jump;
