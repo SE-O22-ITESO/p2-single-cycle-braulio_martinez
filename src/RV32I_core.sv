f50ede089665ea4aff2f03482d3a802cbe252b73
Braulio Isaac Martinez Aceves <braulio.martinez@iteso.mx>
2023-03-25 16:59:41 -0600

    Single cycle changes for core: - Remove _s[0-9] signals (stages of multicycle) - Add dedicated PC+1 adder

--- src/RV32I_core.sv
+++ src/RV32I_core.sv
@@ -15,18 +15,21 @@ module RV32I_core (
 `include "RV32I_core_flops.sv"
 
 // bus_addr selection
-`MUX_2_TO_1(alu_out_s3, program_counter_s1, bus_addr_select_alu_out, bus_addr)
+`MUX_2_TO_1(alu_out, program_counter, bus_addr_select_alu_out, bus_addr)
 // bus_wrdata selection
 always_comb
-    case (mnemonic_s2)
-        SB  : bus_wrdata <= { {24{rs2_s2[7]}}, rs2_s2[7:0]};
-        SH  : bus_wrdata <= { {16{rs2_s2[15]}}, rs2_s2[15:0]};
-        SW  : bus_wrdata <= rs2_s2;
+    case (mnemonic)
+        SB  : bus_wrdata <= { {24{rs2[7]}}, rs2[7:0]};
+        SH  : bus_wrdata <= { {16{rs2[15]}}, rs2[15:0]};
+        SW  : bus_wrdata <= rs2;
         default : bus_wrdata <= '0;
     endcase
 
+// Program counter adder
+assign program_counter_plus_4 = program_counter + 'h4;
+
 RV32I_decoder decoder (
-    .raw_bits   (raw_bits_s1),
+    .raw_bits   (raw_bits),
 
     .rs1_addr   (rs1_addr),
     .rs2_addr   (rs2_addr),
@@ -39,8 +42,8 @@ RV32I_decoder decoder (
 control_unit  control_unit  (
     .clk        (clk),
     .rst        (rst),
-    .opcode     (opcode_s2),
-    .mnemonic   (mnemonic_s2),
+    .opcode     (opcode),
+    .mnemonic   (mnemonic),
 
     .bus_addr_select_alu_out        (bus_addr_select_alu_out),
     .bus_wren   (bus_wren),
@@ -51,11 +54,11 @@ control_unit  control_unit  (
 );
 
 alu_inputs_mux alu_inputs_mux (
-    .rs1        (rs1_s2),
-    .rs2        (rs2_s2),
+    .rs1        (rs1),
+    .rs2        (rs2),
     .imm        (imm),
-    .opcode     (opcode_s2),
-    .program_counter    (program_counter_s1),
+    .opcode     (opcode),
+    .program_counter    (program_counter),
     .control_unit_state (control_unit_state),
 
     .alu_a      (alu_a),
@@ -65,33 +68,33 @@ alu_inputs_mux alu_inputs_mux (
 RV32I_alu alu (
     .a          (alu_a),
     .b          (alu_b),
-    .rs1        (rs1_s2),
-    .rs2        (rs2_s2),
-    .imm        (imm_s2),
+    .rs1        (rs1),
+    .rs2        (rs2),
+    .imm        (imm),
     // Force ALU to ADD during other stages for PC+1
-    .mnemonic   ((control_unit_state == EXECUTE_S3) ? mnemonic_s2 : ADD),
+    .mnemonic   (mnemonic),
 
     .cond_jump  (cond_jump),
     .out        (alu_out)
 );
 
 program_counter_inputs_mux program_counter_inputs_mux (
-    .program_counter_plus_4     (program_counter_plus_4_s1),
+    .program_counter_plus_4     (program_counter_plus_4),
     .program_counter_plus_imm   (program_counter_plus_imm_s2),
-    .alu_out                    (alu_out_s3),
-    .opcode                     (opcode_s2),
+    .alu_out                    (alu_out),
+    .opcode                     (opcode),
     .cond_jump                  (cond_jump),
 
     .program_counter_new        (program_counter_new)
 );
 
 rf_inputs_mux rf_inputs_mux (
-    .alu_out    (alu_out_s3),
-    .program_counter_plus_4 (program_counter_plus_4_s1),
+    .alu_out    (alu_out),
+    .program_counter_plus_4 (program_counter_plus_4),
     .program_counter_plus_imm (program_counter_plus_imm_s2),
-    .opcode     (opcode_s2),
-    .mnemonic   (mnemonic_s2),
-    .bus_rddata (bus_rddata_s4),
+    .opcode     (opcode),
+    .mnemonic   (mnemonic),
+    .bus_rddata (bus_rddata),
 
     .rf_write_data  (rf_write_data)
 );
@@ -103,7 +106,7 @@ RV32I_register_file #  (
     .clk        (clk),
     .rst        (rst),
     .wr_enable  (rf_wren),
-    .wr_addr    (rd_addr_s2),
+    .wr_addr    (rd_addr),
     .wr_data    (rf_write_data),
     .rd_addr_1  (rs1_addr),
     .rd_addr_2  (rs2_addr),
