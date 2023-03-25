6ff9bd15a05fbfbf8aaee8ccdedbc9dbd4143456
Braulio Isaac Martinez Aceves <braulio.martinez@iteso.mx>
2023-03-25 16:58:46 -0600

    Core flops/internal signals: Cleanup for single cycle

--- src/RV32I_core_flops.sv
+++ src/RV32I_core_flops.sv
@@ -1,24 +1,29 @@
 // FETCH_S1 STAGE
-`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), bus_rddata, raw_bits_s1)
-`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), alu_out, program_counter_plus_4_s1)
+/* `FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), bus_rddata, raw_bits_s1)
+`FF_D_RST_EN(clk, rst, (control_unit_state == FETCH_S1), alu_out, program_counter_plus_4_s1) */
 
 
 // DECODE_S2 STAGE
-`FF_D_RST(clk, rst, rd_addr, rd_addr_s2)
-`FF_D_RST(clk, rst, opcode, opcode_s2)
-`FF_D_RST(clk, rst, mnemonic, mnemonic_s2)
+/* `FF_D_RST_DATA_TYPE(clk, rst, rd_addr, rd_addr_s2, RV32I_REGISTER_t)
+`FF_D_RST_DATA_TYPE(clk, rst, opcode, opcode_s2, RV32I_OPCODE_t)
+`FF_D_RST_DATA_TYPE(clk, rst, mnemonic, mnemonic_s2, RV32I_INSTRUCTION_MNEMONIC_t) */
 
-`FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), rs1, rs1_s2)
+/* `FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), rs1, rs1_s2)
 `FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), rs2, rs2_s2)
-`FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), imm, imm_s2)
+`FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), imm, imm_s2) */
 
-`FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), alu_out, program_counter_plus_imm_s2)
+/* `FF_D_RST_EN(clk, rst, (control_unit_state == DECODE_S2), alu_out, program_counter_plus_imm_s2) */
 
 // EXECUTE_S3 STAGE
-`FF_D_RST_EN(clk, rst, (control_unit_state == EXECUTE_S3), alu_out, alu_out_s3)
+/* `FF_D_RST_EN(clk, rst, (control_unit_state == EXECUTE_S3), alu_out, alu_out_s3) */
 
 // MEM_S4 STAGE
-`FF_D_RST_EN(clk, rst, (control_unit_state == MEM_S4), bus_rddata, bus_rddata_s4)
+/* `FF_D_RST_EN(clk, rst, (control_unit_state == MEM_S4), bus_rddata, bus_rddata_s4) */
 
 // WRITEBACK_S5 STAGE
-`FF_D_RST_EN_RESET_VALUE(clk, rst, program_counter_wren, program_counter_new, program_counter_s1, `RV32I_INSTRUCTION_WIDTH'h400000)
+/* `FF_D_RST_EN_RESET_VALUE(clk, rst, program_counter_wren, program_counter_new, program_counter_s1, `RV32I_INSTRUCTION_WIDTH'h400000) */
+
+// Raw bits Single Cycle
+`FF_D_RST_DATA_TYPE(clk, rst, bus_rddata, raw_bits, RV32I_OPERAND_t)
+// PC Single Cycle
+`FF_D_RST_RESET_VALUE(clk, rst, program_counter_new, program_counter, `RV32I_INSTRUCTION_WIDTH'h400000)
--- src/RV32I_core_internal_decls.sv
+++ src/RV32I_core_internal_decls.sv
@@ -1,16 +1,17 @@
 RV32I_OPERAND_t                 rs1, rs2, imm, alu_a, alu_b, alu_out;
-RV32I_OPERAND_t                 rs1_s2, rs2_s2, imm_s2;
-RV32I_OPERAND_t                 raw_bits_s1, alu_out_s3;
-RV32I_OPERAND_t                 program_counter_s1, program_counter_new, program_counter_plus_4_s1, program_counter_plus_imm_s2;
+RV32I_OPERAND_t                 program_counter_new;
+
+RV32I_OPERAND_t                 program_counter, program_counter_plus_4;
+RV32I_OPERAND_t                 raw_bits;
+
 RV32I_OPERAND_t                 rf_write_data;
-RV32I_OPERAND_t                 bus_rddata_s4;
-RV32I_OPCODE_t                  opcode, opcode_s2;
-RV32I_REGISTER_t                rs1_addr, rs1_addr_s2;
-RV32I_REGISTER_t                rs2_addr, rs2_addr_s2;
-RV32I_REGISTER_t                rd_addr, rd_addr_s2;
-RV32I_ALU_OP_t                  alu_op, alu_op_s2;
+RV32I_OPCODE_t                  opcode;
+RV32I_REGISTER_t                rs1_addr;
+RV32I_REGISTER_t                rs2_addr;
+RV32I_REGISTER_t                rd_addr;
+RV32I_ALU_OP_t                  alu_op;
 RV32I_CONTROL_UNIT_FSM_t        control_unit_state;
-RV32I_INSTRUCTION_MNEMONIC_t    mnemonic, mnemonic_s2;
+RV32I_INSTRUCTION_MNEMONIC_t    mnemonic;
 
 wire                            rf_wren, bus_addr_select_alu_out;
 wire                            program_counter_wren;
